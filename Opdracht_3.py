b987a7ca9a4ac50645bc2b7503e44033259a798e
FadwaJava <fadwa.elidrissi@contactoffice.be>
2020-12-11 13:16:14 +0100

    opdracht 3 3b en 3c

--- Opdracht_3.py
+++ Opdracht_3.py
@@ -8,4 +8,28 @@ numbers = list(range(100))
 sum = sum(numbers)
 print(sum)
 
+
 #3b
+
+
+print(group_of_people)
+
+# for statement
+new_group = []
+for s in group_of_people:
+    if s.istitle() or s.islower():
+        new_group.append(s.upper())
+print(new_group)
+
+""" met een list comprehension: in een lijn kan ik de  4 lijnen van de for statement vervangen om het zelfde resultaat 
+te krijgen zonder een nieuw list te creeren"""
+
+group_of_people = [s.upper() for s in group_of_people if(s.istitle() or s.islower())]
+print(group_of_people)
+
+
+#3c
+
+list_0_9 = list(range(10))
+list_matrix = [list_0_9 for n in range(10)]
+print(list_matrix)
