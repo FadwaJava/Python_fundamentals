c2bed335503baece6049ed24fb362f6af8b33c04
FadwaJava <fadwa.elidrissi@contactoffice.be>
2020-12-11 15:52:16 +0100

    4

---
+++ Opdracht_7.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, name):
+        self.name = name
+        self.balance = 0.0
+
+    def deposit(number):
+        return balance + number
+
+
+account = BankAccount('Alex')
+print(account.balance)
+print(account.deposit(10))
