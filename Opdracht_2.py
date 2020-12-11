6ee50afd43cc2f239ec9d9bb9a2972f02abd2280
FadwaJava <fadwa.elidrissi@contactoffice.be>
2020-12-11 12:09:51 +0100

    opdracht 2

---
+++ Opdracht_2.py
@@ -0,0 +1,17 @@
+# gewoone list
+from typing import List
+
+my_data = ["data1", "data2", 0.1, 42]
+print(my_data)
+
+# list aanmaken met een list comprehension
+numbers = [n for n in range(1, 1001) if n % 2 == 0]
+print(numbers)
+
+# list met een while statement en append function
+numbers_1 = []
+n = 2
+while n < 1002:
+    numbers_1.append(n)
+    n = n+2
+print(numbers_1)
